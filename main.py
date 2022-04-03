524638944c926124d08e57c9fb238b59b831e491
lukas <lukasloetkolben@gmail.com>
2022-04-03 19:54:47 +0200

    ...

--- main.py
+++ main.py
@@ -1,12 +1,32 @@
 import os
-import shutil
 import subprocess
 import time
 from pathlib import Path
-# URLs
 
+# URLs
+blueline_url = "https://dl.google.com/dl/android/aosp/blueline-pq3a.190801.002-factory-f3d66c49.zip"
+termux_apk_url = "https://github.com/termux/termux-app/releases/download/v0.118.0/termux-app_v0.118.0+github-debug_arm64-v8a.apk"
+magisk_apk_url = "https://github.com/topjohnwu/Magisk/releases/download/v24.2/Magisk-v24.2.apk"
+patched_boot_img_url = "https://transfer.sh/ormqXH/boot_magisk_patched.img"
+userspace_url = "https://transfer.sh/LVVEJE/userspace.tar.gz"
+platform_tools_url = "https://dl.google.com/android/repository/platform-tools_r29.0.5-linux.zip"
 
 BASE = os.getcwd()
+
+fastboot_path = ""
+
+
+def download_platform_tools():
+    file = "platform-tools.zip"
+    folder = "platform-tools"
+
+    if not Path(BASE, folder).is_dir():
+        if not Path(BASE, file).is_file():
+            run_cmd(f"curl -L {platform_tools_url} --output platform-tools.zip")
+
+        run_cmd(f'unzip {Path(BASE, file)}')
+    global fastboot_path
+    fastboot_path = Path(BASE, folder, "fastboot")
 
 def goto_base():
     run_cmd(f'cd {BASE}')
@@ -18,7 +38,7 @@ def adb_reboot_bootloader():
 
 
 def fastboot_reboot_bootloader():
-    _subprocess(['fastboot', 'reboot-bootloader'])
+    _subprocess([fastboot_path, 'reboot-bootloader'])
     time.sleep(5)
 
 
@@ -32,8 +52,9 @@ def run_cmd(cmd: str):
     print(f"RUN {cmd}")
     os.system(cmd)
 
+
 def install_deps():
-    cmd_apt = 'sudo apt install -y wget android-tools-adb fastboot'
+    cmd_apt = 'sudo apt install -y wget curl android-tools-adb'
     run_cmd(cmd_apt)
 
 
@@ -42,54 +63,42 @@ def download_blueline():
     folder = 'blueline-pq3a.190801.002'
 
     if not Path(BASE, file).is_file():
-        run_cmd(f'wget https://dl.google.com/dl/android/aosp/blueline-pq3a.190801.002-factory-f3d66c49.zip')
+        run_cmd(f'wget {blueline_url}')
 
     if not Path(BASE, folder).is_dir():
         run_cmd(f'unzip {Path(BASE, file)}')
 
-def boot_twrp():
-    adb_reboot_bootloader()
-    if not Path(BASE, "twrp-3.2.3-0-blueline.img").is_file():
-        run_cmd(f'wget https://eu.dl.twrp.me/blueline/twrp-3.2.3-0-blueline.img')
-
-    _subprocess(['fastboot', 'boot', 'twrp-3.2.3-0-blueline.img'])
-    time.sleep(20)
-    process = subprocess.Popen(
-        "adb shell",
-        shell=True,
-        stdin=subprocess.PIPE,
-        stdout=subprocess.PIPE
-    )
 
 def install_blueline():
     adb_reboot_bootloader()
+    _subprocess([fastboot_path, 'erase', 'data'])
     folder = 'blueline-pq3a.190801.002'
     bootloader_img = Path(BASE, folder, 'bootloader-blueline-b1c1-0.1-5578427.img')
-    _subprocess(['fastboot', 'flash', 'bootloader', bootloader_img])
+    _subprocess([fastboot_path, 'flash', 'bootloader', bootloader_img])
     fastboot_reboot_bootloader()
     radio_img = Path(BASE, folder, 'radio-blueline-g845-00017-190312-b-5369743.img')
-    _subprocess(['fastboot', 'flash', 'radio', radio_img])
+    _subprocess([fastboot_path, 'flash', 'radio', radio_img])
     fastboot_reboot_bootloader()
+
     update_img = Path(BASE, folder, "image-blueline-pq3a.190801.002.zip")
-    _subprocess(['fastboot', '-w', 'update', update_img])
+    _subprocess([fastboot_path, '-w', 'update', update_img])
 
 
 def root_phone():
     if not Path(BASE, "termux-app_v0.118.0+github-debug_arm64-v8a.apk").is_file():
-        run_cmd(
-            'wget https://github.com/termux/termux-app/releases/download/v0.118.0/termux-app_v0.118.0+github-debug_arm64-v8a.apk')
+        run_cmd(f'wget {termux_apk_url}')
     if not Path(BASE, "Magisk-v24.2.apk").is_file():
-        run_cmd('wget https://github.com/topjohnwu/Magisk/releases/download/v24.2/Magisk-v24.2.apk')
+        run_cmd(f'wget {magisk_apk_url}')
 
     run_cmd('adb install termux-app_v0.118.0+github-debug_arm64-v8a.apk')
     run_cmd('adb install Magisk-v24.2.apk')
     adb_reboot_bootloader()
 
     if not Path(BASE, "boot_magisk_patched.img").is_file():
-        run_cmd(f'wget https://transfer.sh/ormqXH/boot_magisk_patched.img')
+        run_cmd(f'wget {patched_boot_img_url}')
 
-    _subprocess(['fastboot', 'flash', 'boot', 'boot_magisk_patched.img'])
-    _subprocess(['fastboot', 'reboot'])
+    _subprocess([fastboot_path, 'flash', 'boot', 'boot_magisk_patched.img'])
+    _subprocess([fastboot_path, 'reboot'])
 
 
 def adb_superuser():
@@ -105,7 +114,7 @@ def adb_superuser():
 def install_userspace():
     file = 'userspace.tar.gz'
     if not Path(BASE, file).is_file():
-        run_cmd(f'wget https://dl.google.com/dl/android/aosp/{file}')
+        run_cmd(f'wget {userspace_url}')
 
     run_cmd(f'adb push {BASE}/{file} /sdcard/Download/')
 
@@ -122,7 +131,8 @@ def install_userspace():
                         b"ln -s /data/data/com.termux/files/usr /usr\n"
                         b"exit\n"
                         b"su -c 'HOME=/data/data/com.termux/files/home PATH=\"/data/data/com.termux/files/usr/bin:/bin\" LD_LIBRARY_PATH=\"/data/data/com.termux/files/usr/lib\" bash'\n"
-                        b"mkdir -p /data/data/com.termux/files/tmp/ && mount -t tmpfs -o size=2048M tmpfs /tmp\n"
+                        b"mkdir -p /tmp\n"
+                        b"mount -t tmpfs -o size=2048M tmpfs /tmp\n"
                         b"tmux\n"
                         b"apt-get update\n"
                         b"apt-get install gawk findutils\n"
@@ -138,21 +148,18 @@ def install_userspace():
 
 
 def print_sep():
-    print(42 * "#")
 
+    print(100 * "#")
+    print(100 * "#")
 
-if __name__ == '__main__':
+def run():
+    download_platform_tools()
     print_sep()
     _ = input('Make sure your Bootloader is unlocked.\n'
               'Boot the phone to Android and make sure USB debugging (ADB) is activated!\n'
-              'ALL YOUR DATA WILL BE LOST!\n'
               'Press [ENTER] to confirm.')
     install_deps()
     download_blueline()
-    boot_twrp()
-    _ = input('1.) Go to WIPE\n'
-              '2.) Select "Data" & "Cache" and swipe to wipe!'
-              'Press [ENTER] when you are done.')
     install_blueline()
     print_sep()
     _ = input('WAIT UNTIL YOUR PHONE IS BOOTED!\n'
@@ -170,4 +177,8 @@ if __name__ == '__main__':
     print_sep()
     _ = input('Open Magisk > Superuser and enable Superuser rights of Shell\n'
               'Press [ENTER] when you are done.')
-    install_userspace()
+    #install_userspace()
+
+
+if __name__ == '__main__':
+    run()
